13f8352eac07ea0cb1bb3ca5aadd9e6afd89165c
benaitcheson <benaitcheson@hotmail.com>
2022-02-28 22:21:47 +1000

    didnt work

--- src/lib/firebase.js
+++ src/lib/firebase.js
@@ -4,13 +4,7 @@ import 'firebase/auth';
 import { configs } from 'eslint-plugin-prettier';
 
 const config = {
-  apiKey: "AIzaSyABGvgrES-fWJl_cheJRNCWRNYMKBicUyk",
-  authDomain: "insta-clone-cfc6b.firebaseapp.com",
-  projectId: "insta-clone-cfc6b",
-  storageBucket: "insta-clone-cfc6b.appspot.com",
-  messagingSenderId: "1094668964235",
-  appId: "1:1094668964235:web:092bdcfa9c48f1bddd1f8e",
-  measurementId: "G-5EM9FHWD9C"
+  
 };
 
 const firebase = Firebase.initializeApp(config);
