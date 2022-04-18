175c0c6f8818bb41ee661eced1d0ecf7df6d82c2
benaitcheson <benaitcheson@hotmail.com>
2022-04-18 20:49:52 +1000

    still tying to fix the routing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { lazy, Suspense } from 'react';
+import { React, lazy, Suspense } from 'react';
 import { BrowserRouter as Route, Router, Routes } from 'react-router-dom';
 
 import * as ROUTES from './constants/routes';
